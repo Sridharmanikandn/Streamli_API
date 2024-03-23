a91a4269913cfe3953e78642634ef3a411871234
sridharmanikandn <sridhar.agamworks@gmail.com>
2024-03-23 16:32:58 +0530

    True&Flase_changes_and_Some_other_changes

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ auth_token = authenticate_user()
 if auth_token:
     api_headers_with_token = {"Authorization": f"Bearer {auth_token}","tenantid": "33c75f9f", "appinfo": "CUSTOMER"}
     API_ENDPOINT = "https://api.prac360.com/dashboard/slotreport"
-def fetch_slots():
+def fetch_slots(Slot_view):
     breatheasy='5f0a9c7f624afc79798e66d7'
     chestcare ='a8b466a5ddeeb379556f3024'
     option = st.selectbox(
@@ -74,7 +74,7 @@ def fetch_slots():
         "slotType": ["CUSTOMER"],
         "startDate":start_date,
         "endDate": end_date,
-        "slotView": True
+        "slotView": Slot_view
     }
     response = requests.post(API_ENDPOINT, headers=api_headers_with_token, json=payload)
     print(response)
@@ -83,19 +83,24 @@ def fetch_slots():
 
     if response.status_code == 201:
         data = response.json()
-        if data ["statusCode"] == 1037:
-        #    st.error('No data to show')
-            print("Status error")
-        else: 
-           for slot in data['data']['slots']:
-            slot['date'] = datetime.strptime(slot['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
-            slots_data = data['data']['slots']
-            print(slots_data)
-        if data["statusCode"] == 1001:  
-            return slots_data
+        if data ["statusCode"] == 1001:
+            print("Status error") 
+            if Slot_view == False:
+                json_data= data['data']['slots']
+                print(json_data)
+                booking_per_slot = json_data['bookingPerSlot']
+                booked_appointments = json_data['bookedAppointments']
+                print("Booking Per Slot:", booking_per_slot)
+                print("Booked Appointments:", booked_appointments)
+                return booking_per_slot, booked_appointments
+            else:
+              for slot in data['data']['slots']:
+               slot['date'] = datetime.strptime(slot['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+               slots_data = data['data']['slots']
+               print(slots_data)  
+               return slots_data
         else:
-            # st.error(f"API erroring: {data['msg']}")
-            print("API error")
+            st.warning("No data available for selected dates.")
     else:
         st.error(f"API errors: {response.status_code}")
 
@@ -103,17 +108,30 @@ def fetch_slots():
 
 if selected == "Home":
         st.title(f"you selected the {selected}")
-   
-        slots = fetch_slots()       
-        if slots:
+        option = st.radio("Do you want to see the Total Appointments for selected dates", ("Not-now", "Yes"))
+        if option == "Not-now":
+               value=True
+        elif option == "Yes":
+                value=False
+        result=slots = fetch_slots(Slot_view=value)
+        if isinstance(result, tuple):
+             booking_per_slot, booked_appointments = result
+             print(result)
+             containers = st.container()
+             containers.write("Booking per Appointment")
+             containers.write(booking_per_slot)   
+             containerss = st.container()
+             containerss.write("Total Appointments")
+             containerss.write(booked_appointments)    
+        else:
            st.write("## Slot Booking Summary")
            st.dataframe(slots) 
-        else:
-             st.warning("No data available for selected dates.")
+        
+        
          
 if selected == "Dashboard":
           st.title(f"you selected the {selected}")
-          Chart = fetch_slots()       
+          Chart = fetch_slots(Slot_view=True)       
           if Chart:
            df = pd.DataFrame(Chart)
            charting = alt.Chart(df).mark_bar().encode(
